df3eac10490f4acaea051b139187470db8c642db
jakehorstmann <jake.horstmann@comcast.net>
2024-05-26 13:35:54 -0700

    Added the ability to add categories from the post admin view

--- backend/blogging/admin.py
+++ backend/blogging/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from blogging.models import Post, Category
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInLine]
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ["posts"]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
