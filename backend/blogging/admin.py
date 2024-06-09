8c351186712d73f23ecb3128477a0f422b6e339f
jakehorstmann <jake.horstmann@comcast.net>
2024-06-09 09:30:52 -0700

    Added a Procfile, requirements.txt, and made code style changes

--- backend/blogging/admin.py
+++ backend/blogging/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
 class CategoryInLine(admin.TabularInline):
     model = Category.posts.through
 
+
 class PostAdmin(admin.ModelAdmin):
     inlines = [CategoryInLine]
+
 
 class CategoryAdmin(admin.ModelAdmin):
     exclude = ["posts"]
--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -91,9 +91,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
@@ -119,5 +125,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
